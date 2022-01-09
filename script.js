04148ef21f580e9ea4ed80116569b0bc1e29e778
AshwinKL <kleaaashwin29@gmail.com>
2022-01-09 08:54:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use-strict";
+console.log("running");
+const form = document.querySelector("form");
+const submittedYear = document.querySelector(".input");
+const text = document.querySelector("label");
+const btn = document.querySelector(".btn");
+const reload = document.querySelector("svg");
+
+form.addEventListener("submit", sumbitHandler);
+
+reload.addEventListener("click", () => {
+  location.reload();
+});
+function sumbitHandler(event) {
+  console.log(submittedYear);
+  if (submittedYear.value === "") {
+    return;
+  }
+  const age = 2022 - submittedYear.value;
+  text.textContent = `You are ${age} years old `;
+  btn.classList.add("hidden");
+  event.preventDefault();
+}
